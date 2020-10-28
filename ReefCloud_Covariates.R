b879bb98f6e5b22dee5d8e2a544e9d784e5d4f63
Ronen Galaiduk <garonen@gmail.com>
2020-10-28 17:33:26 +0800

    with calculations for MaxDHW and week numbers

--- ReefCloud_Covariates.R
+++ ReefCloud_Covariates.R
@@ -4,6 +4,7 @@
 # 2. Convert to rasters
 # 3. crop to country's EEZ
 # 4. downscale to the resolution of bathy grids i.e. 1 km
+
 library(ncdf4)
 library(raster)
 library(lubridate)
@@ -13,9 +14,9 @@ library(rts)
 load('F:/ReefCloud/Covariates_ReefCloud/RC_covariates.RData')
 my_wd <- 'F:/ReefCloud/Covariates_ReefCloud'
 if(!dir.exists('./Outputs')) dir.create('./Outputs')
-url_name <- 'https://data.nodc.noaa.gov/thredds/dodsC/cortad/Version6/cortadv6_WeeklySST.nc'
-url_brick <- brick(url_name)
-  attributes(url_brick)
+# url_name <- 'https://data.nodc.noaa.gov/thredds/dodsC/cortad/Version6/cortadv6_WeeklySST.nc'
+# url_brick <- brick(url_name)
+#   attributes(url_brick)
 
 ##### Import bathymetry raster ----------------------------------
   bathy <- raster('F:/ReefCloud/gebco_2020_netcdf/GEBCO_2020.nc')
@@ -25,65 +26,65 @@ z_time <- getZ(url_brick)
   year_id <- which(z_time > as.Date('2002-01-01'))
     rf_brick <- subset(url_brick, year_id)
 # have to reintroduce time into brick as it gets chopped during subsetting
-rf_brick <- setZ(rf_brick, z_time[year_id])
+#rf_brick <- setZ(rf_brick, z_time[year_id])
 # Crop global rasters by a polygon feature of interest----------------------------------
 my_eez <- shapefile('F:/ReefCloud/EEZ_3countries/EEZ_3countries.shp')
-   rf_brick_eez <- 
-     mask(crop(rf_brick,
-               extent(my_eez)),
-               overwrite = T,
-               my_eez,
-               filename = 'F:/ReefCloud/Covariates_ReefCloud/Outputs/Weekly_SST_EEZ.tif'
-     )
-plot(rf_brick_eez[[4]])
-
-
-crop2_time <- setZ(my_eez_crop2,z_time[year_id])
+#    rf_brick_eez <- 
+#      mask(crop(rf_brick,
+#                extent(my_eez)),
+#                overwrite = T,
+#                my_eez,
+#                filename = 'F:/ReefCloud/Covariates_ReefCloud/Outputs/Weekly_SST_EEZ.tif'
+#      )
+# plot(rf_brick_eez[[4]])
+# 
+# 
+# crop2_time <- setZ(my_eez_crop2,z_time[year_id])
 
 
 ###################################################################################################
 #########   Extract relevant months and years from the full brick #################################
 ###################################################################################################
-my_time <- which((month(z_time)== 06 | month(z_time)== 09) & 
-                   (year(z_time) == 2000 | year(z_time) == 2001))
-  print(brick_06 <- subset(url_brick, my_time))
-  brick_06 <- 
-              mask(crop(brick_06,
-               extent(my_eez)),
-               overwrite = T,
-               my_eez#,
-               #filename = 'F:/ReefCloud/Covariates_ReefCloud/Outputs/Weekly_SST_EEZ.tif'
-     )
-  brick_06 <- setZ(brick_06,z_time[my_time])
-plot(brick_06[[2]])
-     
-library(foreach)
-library(doParallel)
-UseCores <- detectCores()-1
-cl <- makeCluster(UseCores)
-registerDoParallel(cl)
-#
-crop_brick <- stack()
-crop_brick <- foreach(i = 1:nlayers(brick_06),
-                     .init = crop_brick,
-                     .combine = addLayer,
-                     .inorder = T,
-                     .packages = c('raster', 'lubridate')) %dopar% {
-    print(i)
-                    
-    my_crop <- mask(crop(brick_06[[i]],
-               extent(my_eez)),
-               my_eez,
-               filename = 'F:/ReefCloud/Covariates_ReefCloud/Outputs/Weekly_SST_EEZ_Crop06.tif')
-    
-}
-crop_brick <- setZ(crop_brick, z_time)                       
-annual_sst_mean <- zApply(crop_brick,  by = year(getZ(crop_brick)), fun = 'mean')- 273.15
-plot(annual_sst_mean[[2]])
-
-monthly_sst_mean <- zApply(crop_brick,  by = month(getZ(crop_brick)), fun = 'mean')- 273.15
-
-stopCluster(cl)
+# my_time <- which((month(z_time)== 06 | month(z_time)== 09) & 
+#                    (year(z_time) == 2000 | year(z_time) == 2001))
+#   print(brick_06 <- subset(url_brick, my_time))
+#   brick_06 <- 
+#               mask(crop(brick_06,
+#                extent(my_eez)),
+#                overwrite = T,
+#                my_eez#,
+#                #filename = 'F:/ReefCloud/Covariates_ReefCloud/Outputs/Weekly_SST_EEZ.tif'
+#      )
+#   brick_06 <- setZ(brick_06,z_time[my_time])
+# plot(brick_06[[2]])
+#      
+# library(foreach)
+# library(doParallel)
+# UseCores <- detectCores()-1
+# cl <- makeCluster(UseCores)
+# registerDoParallel(cl)
+# #
+# crop_brick <- stack()
+# crop_brick <- foreach(i = 1:nlayers(brick_06),
+#                      .init = crop_brick,
+#                      .combine = addLayer,
+#                      .inorder = T,
+#                      .packages = c('raster', 'lubridate')) %dopar% {
+#     print(i)
+#                     
+#     my_crop <- mask(crop(brick_06[[i]],
+#                extent(my_eez)),
+#                my_eez,
+#                filename = 'F:/ReefCloud/Covariates_ReefCloud/Outputs/Weekly_SST_EEZ_Crop06.tif')
+#     
+# }
+# crop_brick <- setZ(crop_brick, z_time)                       
+# annual_sst_mean <- zApply(crop_brick,  by = year(getZ(crop_brick)), fun = 'mean')- 273.15
+# plot(annual_sst_mean[[2]])
+# 
+# monthly_sst_mean <- zApply(crop_brick,  by = month(getZ(crop_brick)), fun = 'mean')- 273.15
+# 
+# stopCluster(cl)
 # system.time({
 # foreach(i = unique(month(getZ(brick_06))), .packages = c('raster', 'lubridate')) %dopar% {
 #           
@@ -109,62 +110,62 @@ stopCluster(cl)
 # #endCluster()
 # monthlies <- zApply(brick_06,  by = month(getZ(brick_06)), fun = 'mean')- 273.15
 # stopCluster(cl)
-
-
-UseCores <- detectCores()-1
-cl <- makeCluster(UseCores)
-registerDoParallel(cl)
-crop_brick <- stack()
-crop_brick <- foreach(i = 1:nlayers(url_brick),
-                     .init = crop_brick,
-                     .combine = addLayer,
-                     .inorder = T,
-                     .packages = c('raster', 'lubridate')) %dopar% {
-    print(i)
-                     }
-    my_crop <- mask(crop(url_brick[[i]],
-               extent(my_eez)),
-               my_eez,
-               filename = 'F:/ReefCloud/Covariates_ReefCloud/Outputs/Weekly_SST_EEZ_Crop.tif')
-    
-}
-crop_brick <- setZ(crop_brick, z_time)                       
-annual_sst_mean <- zApply(crop_brick,  by = year(getZ(crop_brick)), fun = 'mean')- 273.15
-plot(annual_sst_mean[[2]])
-
-monthly_sst_mean <- zApply(crop_brick,  by = month(getZ(crop_brick)), fun = 'mean')- 273.15
-
-stopCluster(cl)
+# 
+# 
+# UseCores <- detectCores()-1
+# cl <- makeCluster(UseCores)
+# registerDoParallel(cl)
+# crop_brick <- stack()
+# crop_brick <- foreach(i = 1:nlayers(url_brick),
+#                      .init = crop_brick,
+#                      .combine = addLayer,
+#                      .inorder = T,
+#                      .packages = c('raster', 'lubridate')) %dopar% {
+#     print(i)
+#                      }
+#     my_crop <- mask(crop(url_brick[[i]],
+#                extent(my_eez)),
+#                my_eez,
+#                filename = 'F:/ReefCloud/Covariates_ReefCloud/Outputs/Weekly_SST_EEZ_Crop.tif')
+#     
+# }
+# crop_brick <- setZ(crop_brick, z_time)                       
+# annual_sst_mean <- zApply(crop_brick,  by = year(getZ(crop_brick)), fun = 'mean')- 273.15
+# plot(annual_sst_mean[[2]])
+# 
+# monthly_sst_mean <- zApply(crop_brick,  by = month(getZ(crop_brick)), fun = 'mean')- 273.15
+# 
+# stopCluster(cl)
 ###################################################################################################
-
-mywd <- 'F:/ReefCloud/Covariates_ReefCloud/Outputs'  
-sst_montmean_brick <- brick()
-for(i in unique(month(z_time[year_id]))) { # will have to adjust here for multiple years 
-  # add doTry and parallel 
-  sst_month <- subset(crop2_time,
-                      which(month(getZ(crop2_time)) == i)) - 273.15 # converting to Celsius
-  print(sst_month) 
-  sst_montmean <- mean(sst_month, na.rm = T)
-  writeRaster(sst_montmean,filename = paste0(mywd, "/",'Mean_Monthly_SST_',i),
-                          format = "GTiff", overwrite = TRUE,
-                          datatype = 'FLT4S', progress = 'text') 
-  
+# 
+# mywd <- 'F:/ReefCloud/Covariates_ReefCloud/Outputs'  
+# sst_montmean_brick <- brick()
+# for(i in unique(month(z_time[year_id]))) { # will have to adjust here for multiple years 
+#   # add doTry and parallel 
+#   sst_month <- subset(crop2_time,
+#                       which(month(getZ(crop2_time)) == i)) - 273.15 # converting to Celsius
+#   print(sst_month) 
+#   sst_montmean <- mean(sst_month, na.rm = T)
+#   writeRaster(sst_montmean,filename = paste0(mywd, "/",'Mean_Monthly_SST_',i),
+#                           format = "GTiff", overwrite = TRUE,
+#                           datatype = 'FLT4S', progress = 'text') 
+#   
   # sst_montmean_res <- resample(sst_montmean, gebco_grid_crop,
   #                                  method = 'bilinear',
   #                                  filename = paste0(mywd, "/",'Mean_Monthly_SST_',i),
   #                                  format = "GTiff", overwrite = TRUE,
   #                                  datatype = 'FLT4S', progress = 'text')
-  print(sst_montmean)                             
-  print(sst_montmean_brick <- brick(sst_montmean,sst_montmean_brick))
-}         
-
-plot(sst_montmean_brick)
-writeRaster(sst_montmean_brick, filename = )
-
-
-zoo <- zApply(crop2_time, by=month(getZ(crop2_time)), fun = 'mean', name='month' )
-plot(zoo)
-crop2_time[[1]]
+#   print(sst_montmean)                             
+#   print(sst_montmean_brick <- brick(sst_montmean,sst_montmean_brick))
+# }         
+# 
+# plot(sst_montmean_brick)
+# writeRaster(sst_montmean_brick, filename = )
+# 
+# 
+# zoo <- zApply(crop2_time, by=month(getZ(crop2_time)), fun = 'mean', name='month' )
+# plot(zoo)
+# crop2_time[[1]]
 
 # mean1 <- raster('F:/ReefCloud/Covariates_ReefCloud/Outputs/Mean_Monthly_SST_1.tif')
 # plot(mean1)
@@ -172,8 +173,8 @@ crop2_time[[1]]
 ##################################################################################################
 
 ##### calculate mean by year---------------------------------------------------------
-mean_99 <- mean(brick99)- 273.15
-plot(mean_99)
+# mean_99 <- mean(brick99)- 273.15
+# plot(mean_99)
 
 
 # 
@@ -213,28 +214,36 @@ ssta_time <- getZ(ssta_brick)
 # have to reintroduce time into brick as it gets chopped during subsetting
 ssta_brick_sub <- setZ(ssta_brick_sub, ssta_time[year_idSSTA])
 # Crop global rasters by a polygon feature of interest----------------------------------
-ssta_crop <- brick()
-for (i in 1:nlayers(ssta_brick_sub))
-  {
-  print(i)
-   ssta_brick_pal <- 
-    mask(crop(ssta_brick_sub[[i]],
-          extent(pal)),
-           overwrite = T,
-            pal
-  )
-print(ssta_crop <- brick(ssta_brick_pal,ssta_crop))
-}
-
-plot(ssta_crop[[2:5]])
+# ssta_crop <- brick()
+# for (i in 1:nlayers(ssta_brick_sub))
+#   {
+#   print(i)
+#    ssta_brick_pal <- 
+#     mask(crop(ssta_brick_sub[[i]],
+#           extent(pal)),
+#            overwrite = T,
+#             pal
+#   )
+# print(ssta_crop <- brick(ssta_brick_pal,ssta_crop))
+# }
+# 
+# plot(ssta_crop[[2:5]])
 # have to reintroduce the time
-print(ssta_crop <- setZ(ssta_crop, ssta_time[year_idSSTA]))
+#print(ssta_crop <- setZ(ssta_crop, ssta_time[year_idSSTA]))
 ##### now calculate annual means of SSTA in degrees Kelvin--------------
-ssta_annual <- zApply(ssta_crop, by = year(getZ(ssta_crop)), fun = 'mean', name = 'annTemp')
-plot(ssta_annual)
+# ssta_annual <- zApply(ssta_crop, by = year(getZ(ssta_crop)), fun = 'mean', name = 'annTemp')
+# plot(ssta_annual)
 ##### and resample the brick to the resolution of the bathy raster
 bathy_pal <- mask(crop(bathy, extent(pal)),pal)
 plot(bathy_pal)
+
+writeRaster(bathy_pal,
+            'F:/ReefCloud/Covariates_ReefCloud/Outputs/Bathymetry/Bathymetry_Palau.tif',
+             overwrite = T,
+             progress = 'text',
+             format = 'GTiff',
+             datatype = 'FLT4S',
+            )
 
 ssta_annual1km <- resample(ssta_annual, bathy_pal, method = 'bilinear') # can write this stack out 
 # but first have to sort out Z values because they get cut out all the time
@@ -259,19 +268,19 @@ plot(tsa_brick[[995]])
 tsa_brick_sub <- subset(tsa_brick, year_idSSTA)
   tsa_brick_sub <- setZ(tsa_brick_sub, ssta_time[year_idSSTA])
 # Crop global rasters by a polygon feature of interest----------------------------------
-tsa_crop <- brick()
-for (i in 1:nlayers(tsa_brick_sub)){
-  tsa_brick_pal <- 
-    mask(crop(tsa_brick_sub[[i]],
-          extent(pal)),
-           overwrite = T,
-            pal
-  )
-print(tsa_crop <- brick(tsa_brick_pal,tsa_crop))
-}
-plot(tsa_crop[[2:5]])
+# tsa_crop <- brick()
+# for (i in 1:nlayers(tsa_brick_sub)){
+#   tsa_brick_pal <- 
+#     mask(crop(tsa_brick_sub[[i]],
+#           extent(pal)),
+#            overwrite = T,
+#             pal
+#   )
+# print(tsa_crop <- brick(tsa_brick_pal,tsa_crop))
+# }
+# plot(tsa_crop[[2:5]])
 # have to reintroduce the time
-print(tsa_crop <- setZ(tsa_crop, ssta_time[year_idSSTA]))
+#print(tsa_crop <- setZ(tsa_crop, ssta_time[year_idSSTA]))
 ##### now calculate annual means of SSTA in degrees Kelvin--------------
 tsa_annual <- zApply(tsa_crop, by = year(getZ(tsa_crop)), fun = 'mean')
 plot(tsa_annual)
@@ -421,6 +430,141 @@ names(ssta_dhwAnn_1km)  <- year(ssta_dhw_ann@time) # have to name the raster by 
              suffix = year(ssta_dhw_ann@time)
            
            )
+ 
+ 
+######################################################################################################
+maxbrick <- brick()
+max_quantyr <- brick () 
+#mod_year <- data.frame(col = m_wks_df)
+mod_year <- list()
+for (i in unique(year(getZ(ssta_dhw_pal)))) {
+
+ts_year <- subset(ssta_dhw_pal, which(year(getZ(ssta_dhw_pal)) == i))
+# calculate to quantile to figure out the DHW
+yr_quant <- calc(ts_year, fun = function(x,...) as.numeric(quantile(x, probs = 0.75,...)), na.rm= T)
+# calculate maximum DHW per year
+yr_max <- calc(ts_year, fun = max, na.rm = T)
+# set NA's to a values otherwise which.max()doesn't work
+ts_year[is.na(ts_year)] <- -9999 
+
+# m <- maxValue(ts_year)
+# mod_year <- rbind(mod_year, data.frame(m))
+# print(colnames(mod_year) <- as.character(i))
+#
+# create raster of week numbers that max DHW occurred in
+max_year <- calc(ts_year, function (x) {which.max(x)})
+names(max_year)<- as.character(i)
+print(maxbrick <- brick(max_year, maxbrick))
+
+names(yr_quant)<- as.character(i)
+print(max_quantyr <- brick(yr_quant, max_quantyr))
+#
+m_wks <- max_year[yr_max > maxValue(yr_quant)] 
+#m_wks_df <- data.frame(m_wks)
+#colnames(m_wks_df) <- paste(as.character(i))
+#mod_year <- cbind(mod_year, m_wks_df)
+#m_wks$i <- as.character(i)
+print(mod_year[[i]] <- m_wks)
+names(mod_year)[i] <- as.character(i)
+# m <- maxValue(ts_year)
+# mod_year <- cbind(mod_year, m)
+#print(colnames(mod_year) <- as.character(i))
+# can instead use some variation of maxValue()
+# hist(m_wks)
+# mean_wks <- as.integer(mean(m_wks))
+#modal(m_wks)
+# ci_wks <- as.integer()
+# modal(maxValue(ts_year))
+# mod <- calc(ts_year, fun = modal, na.rm = T)
+}
+
+plot(maxbrick[[28:38]])
+plot(max_quantyr[[28:38]])
+
+vals <- do.call(cbind, mod_year)
+length(vals)
+attributes(vals$`1980`)
+hist(vals[,'1985'])
+#
+structure(vals)
+par(mfrow = c(2,1))
+plot(x= vals$Week, y= vals$MaxDHW, type = 'l')
+hist(m_wks)
+#########################
+  
+#  max_ssta <- function(ts, year){
+# year <- as.integer(year(getZ(ssta_dhw_pal)))
+# ts_year <- subset(ssta_dhw_pal, (grep(year, names(ssta_dhw_pal))))
+# ts_year[is.na(ts_year)] <- -9999
+# m_ord2 <- calc(ts_year, function (x) {which.max(x)})
+#   which.max(m_ord2)
+#   
+# }
+# summary(ts_year)
+
+
+hist(m_ord2)
+print(m_ord3 <- m_ord2[m_ssta1> 4])
+hist(m_ord3)
+as.integer(mean(m_ord3))
+plot(ts_year[[]])
+mode(values(m_ord3))
+mode(m_ord3)
+
+#X1 <- max_ssta(ssta_dhw_pal,1990)    
+    
+# values(ts_year)    
+#     
+#         
+#     m_ord1 <- calc(ts_year, fun = function(x, na.rm) x[order(x, decreasing = T)][1:4])
+#     which.max(m_ord1)
+#     
+# #    
+# 
+# 
+# for (i in unique(year(getZ(ssta_dhw_pal)))) {
+# 
+#   myyear <- subset(ssta_dhw_pal, which(year(getZ(ssta_dhw_pal)) == i))
+#  print(myyear)
+#   
+# zz <- range(cellStats(myyear,range))
+# m_ord <- calc(myyear, fun = function(x, na.rm) x[order(x, decreasing = T)][1:4])
+# #print(m_ssta1 <- calc(myyear, fun = max, na.rm = T))
+# print(m_ord)
+# which(myyear) == m_ord
+# 
+# }
+#  plot(m_ord,zlim = zz)
+# names(ssta_dhw_pal)
+#  unique(m_ord) 
+# #  
+# plot(m_ssta1)
+# m_ssta1 <- calc(ssta_dhw_pal, fun = max, na.rm = T)
+# maxBands = unique(m_ssta1)
+# outRas = ssta_dhw_pal[[1]]
+# for (i in 1:length(maxBands)) {
+#   myBand = maxBands[i]
+#   outRas[which(ssta_dhw_pal) == myBand] <- ssta_dhw_pal[[myBand]][ssta_dhw_pal == myBand]
+# }
+# 
+# 
+# m_ssta <- raster::calc(ssta_dhw_pal, function(x,...) 
+#                                      max(x, unique(year(getZ(ssta_dhw_pal))),
+#                                      na.rm = T)) 
+# m_ssta
+# 
+# plot(m_ssta1) 
+# 
+# m_ssta <- zApply(ssta_dhw_pal,
+#                  by = year(getZ(ssta_dhw_pal)),
+#                  fun = 'max')
+# values(m_ssta)
+# values(ssta_dhw_pal[[1980]]) 
+# m_ssta <- apply.yearly(ssta_dhw_rs, FUN = )
+# findMethods(which.max)
+# unique(year(getZ(ssta_dhw_pal)))
+# endpoints(ssta_rs, 'years')
+# which(max(ssta_dhw_pal))
 # install.packages('remote')  
 # remotes::install_github("RS-eco/processNC", build_vignettes=T)
 # library(processNC)  
@@ -510,6 +654,7 @@ print(tsa_dhwAnn_1km <- raster::resample(tsa_dhw_pal, bathy_pal, method = 'ngb',
 
 ### Convert to TS object
 tsa_dhw_rs <- rts(tsa_dhwAnn_1km, getZ(tsa_brick))
+plot(tsa_dhw_rs[[30:38]])
 
 tsa_dhw_ann <- apply.yearly(tsa_dhw_rs, FUN= max, na.rm = T) # keep all years in the time series
 # because sometimes it can have effect since 2 years before the current year as with TSA frequency
@@ -528,6 +673,17 @@ writeRaster(tsa_dhw_ann@raster,
              bylayer = T,
              suffix = year(tsa_dhw_ann@time)
             )
+
+####### calculating dates for max dhw-----------
+
+max_dhw <- function(x, t = 6){which(x > t)}
+
+
+m_dhw <- calc(tsa_dhw_pal, fun = max_dhw)
+
+
+m_dhw <- raster::calc(tsa_dhw_pal, function(x) max(x, na.rm = T))
+
 #####################################################################################################
 ##########  Frequency of TSA Anomalies >= 1 deg C over previous 52 weeks-----------------------
 ### so in this case i can find the last date of that year and and use
