6e2d90d86be53efe2b34efc3052deeb432cc5ec1
garonen <garonen@gmail.com>
2020-12-08 16:21:01 +0800

    Finished mean SST function

---
+++ Packages_RC_V2.R
@@ -0,0 +1,10 @@
+library(raster)
+library(ncdf4)
+library(rts)
+library(lubridate)
+
+wd_bathy <- raster('F:/ReefCloud/gebco_2020_netcdf/GEBCO_2020.nc')
+wd_eez <- shapefile('F:/ReefCloud/EEZ_3countries/EEZ_3countries.shp')
+
+my_eez <- subset(wd_eez, ISO_Ter1 == 'PLW')
+my_bathy <- mask(crop(wd_bathy, extent(my_eez)),my_eez)
--- RC_SST_Mean_Cortad.R
+++ RC_SST_Mean_Cortad.R
@@ -6,40 +6,37 @@ my_varname <- 'FilledSST'
 
 RC_SST_Mean_Cortad <- function(url, varname){
   
-  # set designated wd
-  wd <- 'F:/ReefCloud/Covariates_ReefCloud/RC_Outputs'
-  setwd(wd)
-  my_wd <- paste0(wd, '/', my_varname)
-  # and create outputs folder
-  if(!dir.exists(my_wd)) dir.create(my_wd)
-   
-  sst_crop <- mask(
-              crop(
-              brick(url,varname),
-                    extent(my_eez)),
-                    my_eez,
-                    overwrite=T,
-                    progress = 'text',
-                    na.rm = T)
+#set designated wd------
+out_dir <- 'F:/ReefCloud/Covariates_ReefCloud/RC_Outputs'
+  my_wd <- paste0(out_dir, '/', my_varname)
+#and create outputs folder which will be rewritten with every iteration of the function----
+if(!dir.exists(my_wd)) dir.create(my_wd, showWarnings = F, recursive = T )
   
-  sst_mean_cel <- calc((sst_crop)- 273.15, fun= mean, na.rm = T)
-    sst_mean_1k <- raster::resample(sst_mean_cel, my_bathy, method = 'bilinear')
-      sst_mean <- trim(sst_mean_1k,
-                       values = NA)
-      
-      if(!dir.exists(paste0(my_wd)))
-        
-        
-      writeRaster(sst_mean,
-                  filename = paste0(my_wd,'/', 'Mean_', my_varname),
-                  overwrite = T,
-                  na.rm =T,
-                  progress = 'text',
-                  format = 'GTiff',
-                  datatype = 'FLT4S')
+#brick of global netcdfs, crop and mask the area of interest based on the EEZ----
+sst_crop <- mask(
+            crop(
+            brick(url,varname),
+                 extent(my_eez)),
+                        my_eez,
+                        overwrite=T,
+                        progress = 'text',
+                        na.rm = T)
+#convert Kelvin to Celsius, and downscale to the bathymetry raster resolution----
+sst_mean_cel <- calc((sst_crop)- 273.15, fun= mean, na.rm = T)
+  sst_mean_1k <- raster::resample(sst_mean_cel, my_bathy, method = 'bilinear')
+    sst_mean <- trim(sst_mean_1k, values = NA)
+    
+#save to the created folder----
+writeRaster(sst_mean,
+            filename = paste0(my_wd,'/', 'Mean_', my_varname),
+            overwrite = T,
+            na.rm =T,
+            progress = 'text',
+            format = 'GTiff',
+            datatype = 'FLT4S')
       
   plot(sst_mean)
-  print(paste0("Location : ", my_wd))
+    print(paste0("Location : ", my_wd))
 }
 
 RC_SST_Mean_Cortad(my_url, my_varname)
