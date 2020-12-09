00388bd67cafc2cdd45dbee1a5ee4afc22858f03
garonen <garonen@gmail.com>
2020-12-09 15:47:42 +0800

    Auxilary script to Cortad functions

--- Packages_RC_V2.R
+++ Packages_RC_V2.R
@@ -6,5 +6,6 @@ library(lubridate)
 wd_bathy <- raster('F:/ReefCloud/gebco_2020_netcdf/GEBCO_2020.nc')
 wd_eez <- shapefile('F:/ReefCloud/EEZ_3countries/EEZ_3countries.shp')
 
-my_eez <- subset(wd_eez, ISO_Ter1 == 'PLW')
+country = 'PLW'
+my_eez <- subset(wd_eez, ISO_Ter1 == country)
 my_bathy <- mask(crop(wd_bathy, extent(my_eez)),my_eez)
