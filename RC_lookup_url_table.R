fa3c96509c0c345e6f14e95a391e8c6bd49b75e4
garonen <garonen@gmail.com>
2020-12-17 15:39:58 +0800

    Matching url and varname to Cortad

---
+++ RC_lookup_url_table.R
@@ -0,0 +1,44 @@
+RC_lookup_url_table <- function(varname) {
+if (missing(varname)) {
+  stop(paste0("Please type variable name. Choose one of the options:
+              'MeanSST','MeanSummerSST','MaxSSTA_DHW','SSTA_Freq','MaxTSA_DHW',
+              'TSA_Freq','MaxWeek_SSTA','MaxWeek25_SSTA','MaxWeek_TSA','MaxWeek25_TSA'"))
+} else{
+  d_f <- data.frame(url = c(
+    'https://www.ncei.noaa.gov/thredds-ocean/dodsC/cortad/Version6/cortadv6_FilledSST.nc',
+    'https://www.ncei.noaa.gov/thredds-ocean/dodsC/cortad/Version6/cortadv6_FilledSST.nc',
+    'https://www.ncei.noaa.gov/thredds-ocean/dodsC/cortad/Version6/cortadv6_SSTA.nc',
+    'https://www.ncei.noaa.gov/thredds-ocean/dodsC/cortad/Version6/cortadv6_SSTA.nc',
+    'https://www.ncei.noaa.gov/thredds-ocean/dodsC/cortad/Version6/cortadv6_TSA.nc',
+    'https://www.ncei.noaa.gov/thredds-ocean/dodsC/cortad/Version6/cortadv6_TSA.nc',
+    'https://www.ncei.noaa.gov/thredds-ocean/dodsC/cortad/Version6/cortadv6_SSTA.nc',
+    'https://www.ncei.noaa.gov/thredds-ocean/dodsC/cortad/Version6/cortadv6_SSTA.nc',
+    'https://www.ncei.noaa.gov/thredds-ocean/dodsC/cortad/Version6/cortadv6_TSA.nc',
+    'https://www.ncei.noaa.gov/thredds-ocean/dodsC/cortad/Version6/cortadv6_TSA.nc'),
+  cortad_covariate = c(
+      'FilledSST',
+      'FilledSST',
+      'SSTA_DHW',
+      'SSTA_Frequency',
+      'TSA_DHW',
+      'TSA_Frequency',
+      'SSTA_DHW',
+      'SSTA_DHW',
+      'TSA_DHW',
+      'TSA_DHW'),
+  target = c(
+      'MeanSST',
+      'MeanSummerSST',
+      'MaxSSTA_DHW',
+      'SSTA_Freq',
+      'MaxTSA_DHW',
+      'TSA_Freq',
+      'MaxWeek_SSTA',
+      'MaxWeek25_SSTA',
+      'MaxWeek_TSA',
+      'MaxWeek25_TSA'
+    ))
+  d_f[d_f$target == varname, c('url','cortad_covariate')]
+
+  }
+}  
